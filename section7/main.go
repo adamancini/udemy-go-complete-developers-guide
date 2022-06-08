80d35d38927be7bd748c1ee140c731a1a6b89b94
ada mancini <adab3ta@gmail.com>
2022-06-08 00:19:37 -0400

    ch 77 receiving messages

--- section7/main.go
+++ section7/main.go
@@ -13,18 +13,27 @@ func main() {
 		"http://google.com",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		// checkLink(link)
-		go checkLink(link)
+		// checkLink(link)  // move to `go checkLInk()`
+		go checkLink(link, c) // spawn a new goroutine when checkLink blocks on http.Get()
+	}
+
+	// fmt.Println(<-c)
+	for i := 0; i < len(links); i++ {
+		fmt.Println(<-c)
 	}
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link) // http.Get() blocks - we want to fix this with goroutines
 	if err != nil {
 		fmt.Println(link, "might be down!")
+		c <- "Might be down I think"
 		return
 	}
 
 	fmt.Println(link, "is up!")
+	c <- "Yep, it's up"
 }
